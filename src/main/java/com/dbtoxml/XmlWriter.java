7f8cc9f99eef3e564d7e83f6e9623bcd7a54dd16
Alexander Pliskin <alexnetmore@gmail.com>
2017-01-22 02:19:29 +0300

    Remove redundant code

--- src/main/java/com/dbtoxml/XmlWriter.java
+++ src/main/java/com/dbtoxml/XmlWriter.java
@@ -3,14 +3,11 @@ package com.dbtoxml;
 import com.dbtoxml.service.XmlWriterService;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class XmlWriter {
 
     public static void main(String[] args) throws IOException {
-//        String fileName = args[0];
-        Scanner sc = new Scanner(System.in);
-        String fileName = sc.nextLine();
+        String fileName = args[0];
         new XmlWriterService().write(fileName);
     }
 }
